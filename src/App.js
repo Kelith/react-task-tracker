850ff92a7af960cce5cbd04025bd7ae41fce5b3e
Kelith <garofalodavide7@gmail.com>
2022-05-18 16:56:43 +0200

    first half of the course

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Tasks from './components/Tasks';
+import { useState } from 'react'
+
 
 function App() {
+  const [tasks, setTasks] = useState([])
+
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.it !== id))
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title="Hello"></Header>
+      { tasks.length > 0 ?
+          <Tasks tasks={tasks} onDelete={deleteTask}/>
+        :  'You completed all your tasks'
+      }
+
     </div>
   );
 }
