78d4a6ff56483dfc1b15aa1df608cd9d9fbc7f7c
Kelith <garofalodavide7@gmail.com>
2022-05-22 12:07:49 +0200

    Add Task

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 import { useState } from 'react'
 
 
-function App() {
-  const [tasks, setTasks] = useState([])
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctor Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true
+    }
+  ])
 
+  const addTask = (task) => {
+    // I don't have a backand for the ids ... that's bad practice but only for the purpose of this tutorial
+    const id = Math.floor(Math.random() * 10000) +1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
 
   const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.it !== id))
+    setTasks(tasks.filter((task) => task.id !== id))
   }
 
   const toggleReminder = (id) => {
@@ -19,12 +33,15 @@ function App() {
   return (
     <div className="container">
       <Header title="Hello"></Header>
-      { tasks.length > 0 ?
-          <Tasks tasks={tasks} 
+      <AddTask onAdd={addTask}/>
+      { tasks.length > 0 ? 
+        (
+          <Tasks 
+          tasks={tasks} 
           onDelete={deleteTask}
           onToggle={toggleReminder}/>
-        :  'You completed all your tasks'
-      }
+        ):(  'You completed all your tasks'
+      )}
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,17 +1,11 @@
-import Task from './Tasks'
+import Task from './Task'
 
 const Tasks = ({ tasks, onDelete, onToggle }) => {
-
   return (
     <>
-        {tasks.map((task) => (
-           <Task 
-                key={task.id}
-                task={task}
-                onDelete={onDelete}
-                onToggle={onToggle}
-            /> 
-        ))}
+      {tasks.map((task, index) => (
+        <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle} />
+      ))}
     </>
   )
 }
